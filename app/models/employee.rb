f995b47fc8210b64c5755301ef8adb1d7489e3f9
Prashanth <prashanthkumar.g@mwebware.com>
2015-11-09 18:03:07 +0530

    .

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ActiveRecord::Base
-   #self.table_name = "public.employees"
+   self.table_name = "public.employees"
   def self.get_employee(username)
     @employee ||= Java::HarbingerSdkData::Employee.withUserName(username, @entity_manager)
     return @employee
