9585fa1be12c5787cbdf43634e0a592de8acb492
Ashish Kumar <ashishkumar@mwebware.com>
2015-11-09 14:53:43 +0530

    session.keys.each do |cur_session_key|
    end

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,7 @@ class SessionsController < ApplicationController
   end
   
   def get_sessions
-    #session[:user_id]=121
-    #session[:user_name] = "Ashish Kumar"
-    #sessions_json = [{:session_user_id=>session[:user_id], :session_user_name=>session[:user_name]}].to_json
-    #puts sessions_json
-    
-   
+
   end
   
   def create
