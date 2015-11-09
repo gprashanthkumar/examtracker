e543e8f9424d50bd1de325d74bb76a82f2071278
Prashanth <prashanthkumar.g@mwebware.com>
2015-11-09 13:48:30 +0530

    .

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,12 @@ class SessionsController < ApplicationController
   end
   
   def get_sessions
-    session[:user_id]=121
-    session[:user_name] = "Ashish Kumar"
-    sessions_json = [{:session_user_id=>session[:user_id], :session_user_name=>session[:user_name]}].to_json
+    #session[:user_id]=121
+    #session[:user_name] = "Ashish Kumar"
+    #sessions_json = [{:session_user_id=>session[:user_id], :session_user_name=>session[:user_name]}].to_json
     #puts sessions_json
+    
+    @x = session.to_list()
   end
   
   def create
