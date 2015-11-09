620d6d1457c0a6080bc18bba77f561fe817d2d6c
Ashish Kumar <ashishkumar@mwebware.com>
2015-11-09 12:50:18 +0530

    sessions_controller and experiment page added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -23,5 +23,25 @@ class HomeController < ApplicationController
   
   def search
   end
+  
+  def experiment
+    #json_text = [{"aaData"=>{:Id =>"Ashish Kumar", :MRN => "7842141835", :ProcDescription=>"aasfsdfsdf"}}].to_json
+    #arary_text = [{"aaData"=>{:Id =>"Ashish Kumar", :MRN => "7842141835", :ProcDescription=>"aasfsdfsdf"}}].to_a
+    #puts array_text
+  end
+  
+  def get_data
+    json_data = {:aaData=> [["1","10516","CT Scan"],["sdf","adsdf","sadfsd"],["sdf","adsdf","sadfsd"]]}.to_json
+    respond_to do |format|
+     format.json { render :json => json_data }
+    end
+  end
+  
+  def get_data1
+    json_data = {:aaData=> [["sdf","adsdf","sadfsd"],["sdf","adsdf","sadfsd"],["sdf","adsdf","sadfsd"]]}.to_json
+    respond_to do |format|
+     format.json { render :json => json_data }
+    end
+  end
 
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+
+class SessionsController < ApplicationController
+  layout 'examtracker_layout'
+  def new
+  end
+  
+  def get_sessions
+    session[:user_id]=121
+    session[:user_name] = "Ashish Kumar"
+    sessions_json = [{:session_user_id=>session[:user_id], :session_user_name=>session[:user_name]}].to_json
+    #puts sessions_json
+  end
+  
+  def create
+    render 'new'
+  end
+
+  def destroy
+  end
+end
+
