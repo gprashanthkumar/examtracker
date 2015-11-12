104acc98ec73c185b17de1b26370df937adc9a3e
Prashanth <prashanthkumar.g@mwebware.com>
2015-11-12 20:03:52 +0530

    resource_name

--- app/models/rad_exam.rb
+++ app/models/rad_exam.rb
@@ -6,7 +6,7 @@ class Rad_Exam < ActiveRecord::Base
     .joins("LEFT JOIN procedures proc on proc.id = rad_exams.procedure_id ")
     .joins("LEFT JOIN resources res on res.id = rad_exams.resource_id ")
     .joins("LEFT JOIN modalities mod on mod.id = res.modality_id ")
-    .select("p.name,p.birthdate,pmrn.mrn,proc.code,proc.description,modality,rad_exams.*")    
+    .select("p.name,p.birthdate,pmrn.mrn,proc.code,proc.description,modality,res.name as resource_name,rad_exams.*")    
   } 
   #scope :join_tech_employees_name, -> { joins("LEFT JOIN patient_mrns pmrn ON pmrn.patient_id = patients.id" ) }
   #scope :join_patient_mrns, -> { joins("LEFT JOIN patient_mrns pmrn ON pmrn.patient_id = patients.id" ) }
