7e8367f3cdb93be6628d78dd8b7002654da67aa4
Prashanth <prashanthkumar.g@mwebware.com>
2015-11-13 18:17:08 +0530

    .

--- app/models/rad_exam.rb
+++ app/models/rad_exam.rb
@@ -53,10 +53,11 @@ class Rad_Exam < ActiveRecord::Base
   end
   def self.get_tech_exams(employeeid)
     #self.where({patient_mrn_id: mrn}).order("created_at desc").first
-    s = self.join_Main.Radiologist
+    s = self.Radiologist
    #s.where("name ILIKE ?", wildcard_name) unless params[:patient_name].blank?
    s.where("rad_reports.rad1_id = ? or rad_reports.rad2_id = ? or rad_reports.rad3_id = ? or rad_reports.rad4_id = ? ", employeeid, employeeid, employeeid, employeeid) unless employeeid.blank?
     s.order("id desc").all;
+    
     return s
   end
 end
