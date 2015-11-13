2299aeff6e6b44c37beaacdcd1dc3273ea98450d
Prashanth <prashanthkumar.g@mwebware.com>
2015-11-13 19:45:44 +0530

    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -58,6 +58,30 @@ class HomeController < ApplicationController
     end
   end
   
+  def get_jqgrid_common(roletype)
+	#currently this is used to get data for jqgrid_page.
+	@employee = Employee.get_employee(session[:username])   
+  case roletype
+  when 'rad'
+    @exams = Rad_Exam.get_rad_exams(@employee.id)
+    when 'tech'
+    @exams = Rad_Exam.get_tech_exams(@employee.id)
+  when 'tech'  
+  end
+	
+    json_data = {
+		:page=>"1",
+		:total=>"3",
+		:records=>"6", 
+		:rows=> JSON.parse(@exams.to_json(:only => [ :accession,:mrn,:current_status,:code,:description,:modality,:resource_name,:graph_status,:current_status,:updated_at,:patient_name,:birthdate,:site_name,:patient_class,:patient_type,:patient_location_at_exam,:radiology_department,:ordering_provider,:scheduler,:technologist,:pacs_image_count,:appt_time,:sign_in,:check_in,:begin_exam,:end_exam]))    
+    
+    }
+    
+    respond_to do |format|
+     format.json { render :json => json_data }
+    end
+  end
+  
   def get_jqgrid_data
     #working code for jqgrid
     json_data = {
