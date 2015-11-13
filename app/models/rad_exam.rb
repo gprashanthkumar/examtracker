122af0a4e2b9d481768c3ce3e3e34c7bce30068f
Prashanth <prashanthkumar.g@mwebware.com>
2015-11-13 12:06:42 +0530

    bug

--- app/models/rad_exam.rb
+++ app/models/rad_exam.rb
@@ -10,7 +10,7 @@ class Rad_Exam < ActiveRecord::Base
     .joins("LEFT join universal_event_types uet on uet.id = ess.universal_event_type_id ")
     .joins("LEFT join  sites s on s.id = e.site_id")
     .select("p.name,p.birthdate,pmrn.mrn,proc.code,proc.description,modality,res.name as resource_name
-     ,uet.event_type as current_status, ,CASE WHEN s.name IS NULL THEN s.site ELSE s.name END  site_name,rad_exams.*")    
+     ,uet.event_type as current_status,CASE WHEN s.name IS NULL THEN s.site ELSE s.name END  site_name,rad_exams.*")    
   } 
   #scope :join_tech_employees_name, -> { joins("LEFT JOIN patient_mrns pmrn ON pmrn.patient_id = patients.id" ) }
   #scope :join_patient_mrns, -> { joins("LEFT JOIN patient_mrns pmrn ON pmrn.patient_id = patients.id" ) }
