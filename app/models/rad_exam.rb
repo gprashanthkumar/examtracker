6824f4032d99bda540c66b3e7e39b1cfd6d9c947
Prashanth <prashanthkumar.g@mwebware.com>
2015-11-20 15:52:45 +0530

    .

--- app/models/rad_exam.rb
+++ app/models/rad_exam.rb
@@ -109,7 +109,7 @@ class Rad_Exam < ActiveRecord::Base
   end
   
   def self.get_accession_detail(accessionid)
-    accession = self.join_Main.where(" rad_exams.accession = ? ",accessionid).order(rad_exams.id desc).first;
+    accession = self.join_Main.where(" rad_exams.accession = ? ",accessionid).order(id desc).first;
     return accession;
   end
 end
