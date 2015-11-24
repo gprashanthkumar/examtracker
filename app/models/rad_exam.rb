6a2532f3e1b8d14e397a35b7f471c9fe333be8fb
Prashanth <prashanthkumar.g@mwebware.com>
2015-11-24 16:10:57 +0530

    .

--- app/models/rad_exam.rb
+++ app/models/rad_exam.rb
@@ -64,7 +64,7 @@ class Rad_Exam < ActiveRecord::Base
     #self.where({patient_mrn_id: mrn}).order("created_at desc").first
     x = "";
     x = accessions.to_s unless accessions.blank?;
-    puts "filter" + x
+    puts "filter" + x.gsub!("[","").gsub!("]","");
     rad_exams = self.join_Main.Radiologist_Transcript.where("( (rr.rad1_id = ?) or (rr.rad2_id = ?) or  (rr.rad3_id = ?) or (rr.rad4_id = ?)) or (repp.performing_id = ?) ",employeeid,employeeid,employeeid,employeeid,employeeid) .order("id desc").all;              
     #rad_exams = rad_exams.where("accession in (?)",accessions.to_s.gsub!("[","").gsub!("]","")).order("id desc").all unless accessions.blank?; 
     rad_exams = rad_exams.where("accession in ( ? )",x).all unless accessions.blank?; 
