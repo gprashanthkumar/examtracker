483ed7aa603f051fefd0815bef35a0ed0ac00815
Prashanth <prashanthkumar.g@mwebware.com>
2015-11-24 14:23:34 +0530

    fix harbinger issue.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,8 @@
 class Employee < ActiveRecord::Base
    self.table_name = "public.employees"
   def self.get_employee(username)
-    @employee ||= Java::HarbingerSdkData::Employee.withUserName(username, @entity_manager)
+    #@employee ||= Java::HarbingerSdkData::Employee.withUserName(username, @entity_manager)
+    @employee = self.select(employee.*).where("name = ?",usename).first
     return @employee
   end
 end
