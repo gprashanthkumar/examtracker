0029cc4295786e11fe29500905f8177669f6f34b
Prashanth <prashanthkumar.g@mwebware.com>
2015-11-24 16:55:16 +0530

    .

--- app/models/rad_exam.rb
+++ app/models/rad_exam.rb
@@ -79,7 +79,7 @@ class Rad_Exam < ActiveRecord::Base
     rad_exams = self.join_Main.Radiologist_Transcript.where("( (rr.rad1_id = ?) or (rr.rad2_id = ?) or  (rr.rad3_id = ?) or (rr.rad4_id = ?)) or (repp.performing_id = ?) ",employeeid,employeeid,employeeid,employeeid,employeeid) .order("id desc").all;              
     #rad_exams = rad_exams.where("accession in (?)",accessions.to_s.gsub!("[","").gsub!("]","")).order("id desc").all unless accessions.blank?; 
     rad_exams = rad_exams.where("accession in ( " + accfilter +")" ).all unless accessions.blank?; 
-    rad_exams = rad_exams.where("current_status in ( " + csfilter +")" ).all unless current_status.blank?; 
+    rad_exams = rad_exams.where("uet.event_type in ( " + csfilter +")" ).all unless current_status.blank?; 
     
     return rad_exams;
   end
