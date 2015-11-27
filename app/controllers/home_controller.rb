3a2545b04e5f2bb495ab0942a57f682ceef7ee97
Prashanth <prashanthkumar.g@mwebware.com>
2015-11-27 14:27:46 +0530

    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -268,7 +268,7 @@ class HomeController < ApplicationController
     @exams.graph_status = exam.graph_status;
     
     respond_to do |format|
-      format.json { render :json => @exams.to_json(:only => [ :accession,:mrn,:current_status,:code,:description,:modality,:resource_name,:graph_status,:current_status,:updated_at,:patient_name,:birthdate,:site_name,:patient_class,:trauma,:patient_type,:patient_location_at_exam,:radiology_department,:ordering_provider,:scheduler,:technologist,:pacs_image_count,:appt_time,:sign_in,:check_in,:begin_exam,:end_exam,:first_final,:last_final,:order_arrival]) }
+      format.json { render :json => @exams.to_json(:only => [ :accession,:mrn,:current_status,:code,:description,:modality,:resource_name,:graph_status,:current_status,:updated_at,:patient_name,:birthdate,:site_name,:patient_class,:trauma,:patient_type,:patient_location_at_exam,:radiology_department,:ordering_provider,:scheduler,:technologist,:pacs_image_count,:appt_time,:sign_in,:check_in,:begin_exam,:end_exam,:first_final,:last_final,:order_arrival,:rad1_name,:rad2_name]) }
     end    
   end
   
