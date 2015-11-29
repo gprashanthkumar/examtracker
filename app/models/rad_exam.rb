339381ed2a2572858e1fd2126b227a49d269e312
Prashanth <prashanthkumar.g@mwebware.com>
2015-11-29 23:13:00 +0530

    .

--- app/models/rad_exam.rb
+++ app/models/rad_exam.rb
@@ -184,12 +184,12 @@ class Rad_Exam < ActiveRecord::Base
      
     exams_search = self.join_Main;  
     if ((params[:visit] != "") && !(params[:visit].nil?) && !(params[:visit].blank?)) 
-        exams_search = exams_search.join("Left JOIN visits v on v.id = rad_exams.visit_id")
+        exams_search = exams_search.joins("Left JOIN visits v on v.id = rad_exams.visit_id")
         exams_search = exams_search.where(" (v.visit_number ilike ?)  " , "%#{params[:visit]}%" ).all ;
       end
     
      if ((params[:order_id] != "") && !(params[:order_id].nil?) && !(params[:order_id].blank?)) 
-        exams_search = exams_search.join("Left JOIN orders o on o.id = rad_exams.order_id")
+        exams_search = exams_search.joins("Left JOIN orders o on o.id = rad_exams.order_id")
         exams_search = exams_search.where(" (o.order_number ilike ?)  " , "%#{params[:order_id]}%" ).all ;
       end
     
