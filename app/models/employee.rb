b436cd9ed73ef9dff0882405bc5c58a585e37715
Prashanth <prashanthkumar.g@mwebware.com>
2016-01-27 15:29:07 +0530

    .

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,7 @@ class Employee < ActiveRecord::Base
    self.table_name = "public.employees"
   def self.get_employee(username)
     puts username
+	@employee  = nil;
 	@employee ||= Java::HarbingerSdkData::Employee.withUserName(username, @entity_manager)
 	puts "kumar start"
 	puts @employee.to_json
