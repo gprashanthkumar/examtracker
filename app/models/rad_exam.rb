4e9186b2f361907d3e715e0a469fdf5ed213a76b
Prashanth <prashanthkumar.g@mwebware.com>
2016-02-10 18:39:13 +0530

    .

--- app/models/rad_exam.rb
+++ app/models/rad_exam.rb
@@ -288,6 +288,8 @@ class Rad_Exam < ActiveRecord::Base
   def self.testsdkJson
   @mysdk1 = " ";
    q1 = Java::HarbingerSdkData::RadExam.createQuery(@entity_manager)
+   q1.join(".radReports")
+    q1.join("RadReport.radExamId")
    @mysdk1=  q1.list.to_a 
   end
   
