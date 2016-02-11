4474aeb5ea7ab581e922bab86a9ef48cf805c79e
Prashanth <prashanthkumar.g@mwebware.com>
2016-02-11 17:59:39 +0530

    grades

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -119,9 +119,9 @@ class HomeController < ApplicationController
         
         #remove this line after testing
       #<start>
-     exam = manipulate_status(grades);
+     #exam = manipulate_status(grades);
       #<end>
-      exam = get_graph_status(grades);    
+      grades = get_graph_status_hash(grades);    
     
    
     
@@ -409,6 +409,59 @@ class HomeController < ApplicationController
       
     return exam;
   end
+  
+  
+  def get_graph_status_hash(exam={} )
+    gstatus = ""
+    gstatus = exam["graph_status"];
+    exam["graph_status"] ="";
+    if not( (exam["order_arrival"].nil?) || (exam["order_arrival"].blank?))         
+      exam["graph_status"] += "order_time->" + exam["order_arrival"].to_s + "," 
+    else
+      exam["graph_status"]  += "order_time->"  + ","  
+    end
+      
+    if not( (exam["sched_time"].nil?) || (exam["sched_time"].blank?))         
+      exam["graph_status"] += "sched_time->" + exam["sched_time"].to_s + "," 
+    else
+      exam["graph_status"]  += "sched_time->"  + ","  
+    end
+      
+    if not( (exam["appt_time"].nil?) || (exam["appt_time"].blank?))
+      exam["graph_status"] += "appt_time->" + exam["appt_time"].to_s + "," 
+    else
+      exam["graph_status"] += "appt_time->" +  "," 
+    end
+    if not( (exam["sign_in"].nil?) || (exam["sign_in"].blank?))
+      exam["graph_status"] += "sign_in->" + exam["sign_in"].to_s + ","
+    else
+      exam["graph_status"] += "sign_in->" +  ","
+    end  
+    if not( (exam["check_in"].nil?) || (exam["check_in"].blank?))
+      exam["graph_status"] += "check_in->" + exam["check_in"].to_s + ","
+    else
+      exam["graph_status"] += "check_in->"  + ","
+    end  
+    if not( (exam["begin_exam"].nil?) || (exam["begin_exam"].blank?))
+      exam["graph_status"] += "begin_exam->" + exam["begin_exam"].to_s + ","
+    else
+      exam["graph_status"] += "begin_exam->" + ","
+    end  
+    if not( (exam["end_exam"].nil?) || (exam["end_exam"].blank?))
+      exam["graph_status"] += "end_exam->" + exam["end_exam"].to_s + ","
+    else
+      exam["graph_status"] += "end_exam->" +  ","
+    end 
+    if not( (exam["report_time"].nil?) || (exam["report_time"].blank?))
+      exam["graph_status"] += "final_time->" + exam["report_time"].to_s + ","
+    else
+      exam["graph_status"] += "final_time->" +  ","
+    end      
+    exam["graph_status"] += gstatus;
+      
+    return exam;
+  end
+  
   
   def get_accession_report
     @accession_id = params[:accession_id];
