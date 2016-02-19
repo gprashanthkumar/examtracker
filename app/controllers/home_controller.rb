875e5e2aed4706e6cea83e2deda6ba55f6163d7b
Prashanth <prashanthkumar.g@mwebware.com>
2016-02-19 21:02:01 +0530

    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -255,7 +255,7 @@ class HomeController < ApplicationController
       :page=>"1",
       :total=>"3",
       :records=>"6", 
-      :rows=> JSON.parse(@exams)    
+      :rows=> JSON.parse("hello world")    
     
     }
     
