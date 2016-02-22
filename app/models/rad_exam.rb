6b2554623b70692682cc7599cbc63e786524c3f0
Prashanth <prashanthkumar.g@mwebware.com>
2016-02-22 16:29:18 +0530

    .

--- app/models/rad_exam.rb
+++ app/models/rad_exam.rb
@@ -158,6 +158,17 @@ class Rad_Exam < ActiveRecord::Base
     qmyexams =nil;
     qmyorders = nil;       
  
+     
+      #exams_search = exams_search.where("( (rr.rad1_id = ?) or (rr.rad2_id = ?) or  (rr.rad3_id = ?) or (rr.rad4_id = ?)) ",employeeid,employeeid,employeeid,employeeid).all;
+      qmyreports =   
+           q1.or([
+              q1.equal(".currentReport.rad1.id",employeeid),q1.equal(".currentReport.rad2.id",employeeid),q1.equal(".currentReport.rad3.id",employeeid),q1.equal(".currentReport.rad4.id",employeeid)
+                 ].delete_if {myreports != true}
+               );
+               
+        q1.where(q1.and(
+          [qmyreports]
+         ));
     
     puts q1.toSQL;
     return q1.list.to_a ;
