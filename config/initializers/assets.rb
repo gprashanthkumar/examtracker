7aff629030782a38b3dee1533b6156b88468c0d2
Prashanth <prashanthkumar.g@mwebware.com>
2016-03-02 19:16:30 +0530

    .

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -29,7 +29,7 @@ Rails.application.config.assets.precompile += %w( bootstrap.min.css
                                                   jquery-ui-1.10.3.min.js 
                                                   jquery.multiselect.js
 												  appHead1.js
-												  jqgrid/jquery.jqGrid.run.js)
+												  jquery.jqGrid.run.js)
 
 
 #config.assets.precompile += [Rails.root.to_s + '/vendor/assets/stylesheets/custom/*.gif']
