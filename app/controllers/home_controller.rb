0558efe4c20e36794f8b55ce49d13b0e4b116be2
Prashanth <prashanthkumar.g@mwebware.com>
2016-03-10 21:14:40 +0530

    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -385,12 +385,13 @@ class HomeController < ApplicationController
     
     #log output data
     log_hipaa_view(@mysdk1);
-   
+    @pages =  (@mysdkTotal.to_f / @gridRows.to_f);
+    @pages = @pages + 1 unless (@mysdkTotal.to_f%@gridRows.to_f == 0)
     #puts @exams.to_json;
     json_data = {
       :page=> @gridPage.to_s|| "1",
-      :total=> @mysdkTotal.to_s || "10",
-      :records=> @exams.count.to_s|| "10", 
+      :total=> @pages.to_s || "10",
+      :records=> @mysdkTotal.to_s|| "10", 
       #:rows=> JSON.parse(@exams.to_json(:only => [ :accession,:mrn,:current_status,:code,:description,:modality,:resource_name,:graph_status,:current_status,:updated_at,:patient_name,:birthdate,:site_name,:patient_class,:patient_type,:patient_location_at_exam,:radiology_department,:ordering_provider,:scheduler,:technologist,:pacs_image_count,:appt_time,:sign_in,:check_in,:begin_exam,:end_exam]))    
       :rows=> JSON.parse(@exams.to_json)
     }    
