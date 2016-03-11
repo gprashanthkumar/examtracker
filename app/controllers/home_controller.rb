22733b8cde5394181c251a19b0f3896f91821c59
Prashanth <prashanthkumar.g@mwebware.com>
2016-03-11 18:14:47 +0530

    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -368,7 +368,7 @@ class HomeController < ApplicationController
         end  
         puts "<---- start of id list \n"
        @mysdkTotal = Rad_Exam.get_exams_search_by_id_array(idList,true);
-       @mysdk1 = Rad_Exam.get_exams_search_by_id_array(idList,false);
+       @mysdk1 = Rad_Exam.get_exams_search_by_id_array(idList,false,@gridPage,@gridRows);
 
       else
          puts "<---- start of id list \n"
